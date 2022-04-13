655f0b25d44e91a9f5b2b8b37cd63d84909be3cb
Akash Crown <akashcrown28@gmail.com>
2022-04-13 21:00:42 +0530

    changing in email name

--- lib/loginform.dart
+++ lib/loginform.dart
@@ -22,7 +22,7 @@ class _LoginFormState extends State<LoginForm> {
             ),
             TextField(
               decoration: InputDecoration(
-                hintText: 'Email',
+                hintText: 'Email/user name',
                 prefixIcon: Icon(Icons.email),
               ),
             ),
